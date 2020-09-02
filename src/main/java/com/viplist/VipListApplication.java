83b846e3a0d987a920ed302e2a84016d3cad882a
Leonardo Narciso <LNarcisoOV@gmail.com>
2020-09-01 22:26:54 -0300

    First commit.

---
+++ src/main/java/com/viplist/VipListApplication.java
@@ -0,0 +1,13 @@
+package com.viplist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viplist/VipListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viplist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
