107ad6100b1a6e09f0fabbf57d84611b855f4367
Leonardo Narciso <LNarcisoOV@gmail.com>
2020-09-01 23:18:07 -0300

    Adding mapping for vip list.

--- src/main/java/com/viplist/controller/InvitedController.java
+++ src/main/java/com/viplist/controller/InvitedController.java
@@ -4,11 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/")
-public class IndexController {
-	
+@RequestMapping(value = "/invitedlist")
+public class InvitedController {
+
 	@RequestMapping(value="/")
-	public String index() {
-		return "index";
+	public String invitedList() {
+		return "invitedlist";
 	}
+
 }
