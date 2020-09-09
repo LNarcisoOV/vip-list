ef6a58e8a24922d94b41f0527e4e78d6b4364b5d
Leonardo Narciso <LNarcisoOV@gmail.com>
2020-09-08 23:05:31 -0300

    Sending email to inviteds.

--- src/main/java/com/viplist/controller/InvitedController.java
+++ src/main/java/com/viplist/controller/InvitedController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alura.sendemail.service.EmailService;
 import com.viplist.model.Invited;
 import com.viplist.repository.InvitedRepository;
 
@@ -18,11 +21,20 @@ public class InvitedController {
 
 	@Autowired
 	private InvitedRepository invitedRepository;
-	
-	@RequestMapping(value="/")
+
+	@GetMapping(value = "/")
 	public ResponseEntity<List<Invited>> invitedList(Model model) {
 		List<Invited> invitedList = invitedRepository.findAll();
 		return new ResponseEntity<List<Invited>>(invitedList, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/send-email")
+	public ResponseEntity<Object> sendEmail() {
+		List<Invited> invitedList = invitedRepository.findAll();
+		EmailService emailService = new EmailService();
+		invitedList.forEach(inv -> emailService.send(inv.getEmail()));
+		return new ResponseEntity<Object>(HttpStatus.OK);
+		
 	}
 
 }
